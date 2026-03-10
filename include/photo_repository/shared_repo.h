c92b588e25980186b9e968a2b2229c1c285f3f22
uliana <unan0907@gmail.com>
2026-03-09 17:36:07 -0700

    inheritance # Conflicts: #	.idea/nailscpp.iml

--- include/photo_repository/shared_repo.h
+++ include/photo_repository/shared_repo.h
@@ -5,4 +5,13 @@
 #ifndef NAILSCPP_SHARED_REPO_H
 #define NAILSCPP_SHARED_REPO_H
 
+#include "photo_repository/photo_repo.h"
+
+class SharedRepository final : public PhotoRepository {
+public:
+    explicit SharedRepository(std::filesystem::path storageDirectory);
+
+    PhotoId importFromPersonal(const Photo& photo);
+};
+
 #endif //NAILSCPP_SHARED_REPO_H
